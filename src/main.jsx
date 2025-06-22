74a0fbcd35ebb9de238bb55fd49d7c47715d4e0e
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-06-22 20:10:16 +0700

    fix routing page

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import App from './App.jsx' 
 import HomePage from './pages/HomePage.jsx'
-// import ProjectDetail from './pages/ProjectDetail.jsx'
-
-const basename = import.meta.env.PROD ? '/syahreza_adnan_portfolio/' : '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename="/syahreza_adnan_portfolio/">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          {/* <Route path="project/:projectId" element={<ProjectDetail />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
