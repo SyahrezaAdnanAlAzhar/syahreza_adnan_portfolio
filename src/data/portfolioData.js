a7ca7e79b67802579d0f917b9977dfdb5000b149
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-06-22 20:19:44 +0700

    test

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -3,7 +3,7 @@ import { FaGithub, FaLinkedin, FaBehance, FaEnvelope } from 'react-icons/fa';
 export const personalInfo = {
   name: "Syahreza", 
   greeting: "Hi all, I'm",
-  intro: "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks.", 
+  intro: "A passionate Full Stack Software Developer :) 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks.", 
   resumeUrl: "https://bit.ly/cv_syahrezaadnan"
 };
 
