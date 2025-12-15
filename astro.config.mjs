a38927ec93429df7ff211367b0b8b78ae021349e
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2025-12-15 12:44:22 +0700

    init project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://syahrezaadnanalalzhar.github.io',
+  base: '/syahreza_adnan_portfolio',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  output: 'static',
+  build: {
+    assets: '_assets'
+  }
+});
